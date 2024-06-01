b735530b70f9f93a1ecc1b2cc74db911648c1fd1
Sergio_Nahin <sergiontr280@gmail.com>
2024-05-31 23:28:26 -0600

    actualizar pwd y antes de actualizar version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CounterModule } from './counter/counter.module';
 import { AppCounter } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppCounter],
   imports: [
     BrowserModule,
-    CounterModule    
+    CounterModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })    
   ],
   providers: [],
   bootstrap: [AppCounter]
